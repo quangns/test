6051924205611f4defde1d5301e5f776838e0cec
nguyen-sy-quang <quanghoangmang1203@gmail.com>
2016-05-20 16:54:37 +0700

    Mod: su dung ipwhois trong file netname.py

--- rqworker/netname.py
+++ rqworker/netname.py
@@ -1,16 +1,20 @@
-import subprocess
+from ipwhois import IPWhois
 
 
 def analysis(line):
     target = open("netname.txt", 'a')
-    inetnum_ = subprocess.Popen("whois {}".format(line),
-                                shell=True, stdout=subprocess.PIPE)
-    output = inetnum_.communicate()
-    result = str(output).split('\\n')
-    if 'inetnum' in result[5]:
-        for i in range(5, 11):
-            target.write(result[i])
-            target.write("\n")
+    obj = IPWhois(line)
+    res = obj.lookup_whois()
+    result = res['nets'][0]
+    if 'range' in result:
+        if "\n" in result['description']:
+            descrip = result['description'].split('\n')
+            target.write("description:  %s %s\n" % (descrip[0], descrip[-1]))
+        else:
+            target.write("description:  %s %s\n" % (result['description']))
+        target.write("inetnum:      %s\n" % (result['range']))
+        target.write("netname:      %s\n" % (result['name']))
+        target.write("cidr:         %s\n" % (result['cidr']))
         target.write("------------------------------------------"
                      "----------------------------------------\n")
     target.close()
